d48474d13aa7eae99bc976e617087eb6636f6506
Evan Cole <ecole5@uwo.ca>
2021-02-25 10:51:55 -0500

    Testing now working

---
+++ src/test/BinarySearch/SqrtTest.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SqrtTest {
+
+    private int cool;
+
+    @BeforeEach
+    void setUp() {
+        cool = 3;
+    }
+
+    @Test
+    void mySqrt() {
+        System.out.println(cool);
+    }
+}
--- src/test/strings/rotateArrayTest.java
+++ src/test/strings/rotateArrayTest.java
@@ -1,25 +1,25 @@
 package strings;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class rotateArrayTest {
 
-    private rotateArray test;
+    private rotateArray runner;
 
     @BeforeEach
     void setUp() {
-        rotateArray test = new rotateArray();
+        runner = new rotateArray();
     }
 
     @Test
-    @DisplayName("Simple multiplication should work")
     void rotate() {
-        int [] arrray = {1,2,3};
-        test.rotate(arrray,3);
-        assertArrayEquals(new int[] {1,2,3}, arrray);
+        int[] test = {1,2,3};
+        runner.rotate(test,3);
+        assertArrayEquals(new int[]{1,2,3},test);
     }
 }
+
